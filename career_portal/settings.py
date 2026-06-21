5a18f9bc7072b513bc19830853f8fb417543ba71
Ashish Ranjan <iamrashish@gmail.com>
2026-06-21 18:41:40 +0530

    fix allowed hosts

--- career_portal/settings.py
+++ career_portal/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-$ye!plnyv7=+&p5or41m&x^e)kl-56#kpu$12%a_s763mrz13h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1",
-    "localhost",]
+ALLOWED_HOSTS = [   "127.0.0.1",
+    "localhost",
+    "career-portal-pvgr.onrender.com",]
 
 
 # Application definition
